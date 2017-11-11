fed23b322cf4258f1a42d7b234a605f638461f6f
kwame <kwame@192.168.2.23>
2017-11-11 16:56:14 -0500

    Complete

--- src/pkg12u/assignment5/A5Q3.java
+++ src/pkg12u/assignment5/A5Q3.java
@@ -10,26 +10,41 @@ package pkg12u.assignment5;
  */
 public class A5Q3 {
 
-    // Store all the integers on the stack
-    private String[] stack;
+    // Store all the characters on the stack
+    private char[] stack;
 
     // Constructor
     public A5Q3() {
-        stack = new String[10];
+        stack = new char[10];
     }
 
     public boolean inLang(String word) {
-        // Return false if the middle character is not "$"
+        // return false if the middle letter is not '$'
         if (word.charAt(word.length() / 2) != '$') {
             return false;
         }
-        // Go through the word comparing the first and last letters
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+        // while the word is more the one letter
+        if (word.length() > 1) {
+            // Put the characters into a stack
+            for (int i = 0; i < word.length(); i++) {
+                stack[i] = word.charAt(i);
+            }
+            // compare the first and last letters
+            if (stack[0] == stack[word.length() - 1]) {
+                // if they are the same, remove the first and last letter from the word
+                word = word.substring(1, word.length() - 1);
+                // call on the method again with the new, shortened word
+                return inLang(word);
+            } else {
+                // If the first and last letter do not match
                 return false;
             }
+        } else {
+            // If the word length reaches 1
+            return true;
+
         }
-        return true;
+
     }
 
     /**
@@ -38,7 +53,11 @@ public class A5Q3 {
     public static void main(String[] args) {
         // Test the method
         A5Q3 test = new A5Q3();
-        String word = "cat$cat";
+        String word = "cat$tac";
+        System.out.println("The word " + word + " is in the language: " + test.inLang(word));
+        word = "hello";
+        System.out.println("The word " + word + " is in the language: " + test.inLang(word));
+        word = "cattac";
         System.out.println("The word " + word + " is in the language: " + test.inLang(word));
     }
 }
